81dc68f12107d7e10ba714db8c4e4827d99fe536
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-24 00:21:56 +0100

    react part finished

--- react_pwa/src/App.js
+++ react_pwa/src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
 
-      console.log(data);
+      setWeather(data);
+      setQuery("");
     }
   };
   return (
@@ -21,6 +23,24 @@ const App = () => {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+      {weather.main && ( // this means that if weather.main exists, then it's going to return the component below
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;</sup>
+          </div>
+          <img
+            className="city-icon"
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>{weather.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 };
